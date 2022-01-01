7608d8c389582cedad44ad8ba9a14173274406b7
Guillermo Torres Pérez <guilletorresperez@gmail.com>
2022-01-01 20:47:45 +0100

    Update tarifas-comerciales.cpp
    Me he tirado un poco de triple porque no estoy seguro de si he entendi el enunciado pero bueno ahí está :)

--- src/tarifas-comerciales.cpp
+++ src/tarifas-comerciales.cpp
@@ -1,1 +1,30 @@
-//
+#include <string>
+#include "gasto-diario.hpp"
+#include "tarifas-comerciales.hpp"
+#pragma once
+using namespace std;
+
+double costeDiarioTarifaPlanaTramos(const GastoDiario& gasto, 
+                                    const TarifaPlanaTramos tarifa) {
+    double suma = 0;
+    for (unsigned i = 0; i < NUM_HORAS; i++) {
+        if ((i > 10 && i < 14) || (i > 18 && i < 22)) {
+            suma += gasto.consumo[i] * tarifa.punta;
+        } else if ((i > 8 && i < 10) || (i > 14 && i < 18) || (i > 22 && i < 0)) {
+            suma += gasto.consumo[i] * tarifa.llano;
+        } else if (i >  0 && i < 8) {
+            suma += gasto.consumo[i] * tarifa.valle;
+        }
+    }
+    return suma;
+}
+
+double costeTarifaPlanaTramos(const GastoDiario regDiario[],
+                              const unsigned numRegs, const TarifaPlanaTramos tarifa) {
+    double suma = 0;
+    for (unsigned i = 0; i < numRegs; i++) {
+        suma += costeDiarioTarifaPlanaTramos(regDiario[i], tarifa);
+    }
+    return suma;
+    
+}
