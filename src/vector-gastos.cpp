34c8707b5396ad531f3cc0cf018bffc6ec468129
Guillermo Torres Pérez <guilletorresperez@gmail.com>
2022-01-03 18:55:55 +0100

    Función diaMasBarato

--- src/vector-gastos.cpp
+++ src/vector-gastos.cpp
@@ -1,1 +1,20 @@
-//
+#include "gasto-diario.hpp"
+
+void diaMasBarato(const GastoDiario regsDiarios[], const unsigned numRegs,
+                  Fecha& dia, double& precioMedioMinimo) {
+    GastoDiario menorPrecio = regsDiarios[0];
+    for (int i = 1; i < numRegs; i++) {
+        if (costeMedio(menorPrecio) > costeMedio(regsDiarios[i])) {
+            menorPrecio = regsDiarios[i];
+        }
+    }
+    dia.dia = menorPrecio.fecha.dia;
+    precioMedioMinimo = costeMedio(menorPrecio);
+}
+
+void horaMasCara(const GastoDiario regsDiarios[], const unsigned numRegs, 
+                 Fecha& dia, unsigned& hora, double& precioMaximo) {
+    GastoDiario mayorPrecio = regsDiarios[0];
+    for (int i = 1; i < numRegs; i++) {
+    }
+}
